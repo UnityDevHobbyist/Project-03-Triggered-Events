54af4335ed5fc4cb40bc30db5223d6c0e4c9d60e
Kiss <l3alazskiss15@gmail.com>
2022-11-14 21:46:05 -0400

    Added first timeline (finally)

--- Assets/TriggerScript.cs
+++ Assets/TriggerScript.cs
@@ -6,6 +6,12 @@ using UnityEngine.Playables;
 public class TriggerScript : MonoBehaviour
 {
     public PlayableDirector timeline;
+    public GameObject obj;
+
+    void Start()
+    {
+        obj.SetActive(false);
+    }
 
     public bool OneTimeTrigger = false;
 
@@ -13,6 +19,8 @@ public class TriggerScript : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            obj.SetActive(true);
+
             timeline.Play();
 
             if (OneTimeTrigger == true)
@@ -20,9 +28,15 @@ public class TriggerScript : MonoBehaviour
                 Destroy(this.gameObject);
             }
 
+            timeline.stopped += OnPlayableDirectorStopped;
+
         }
     }
 
+    void OnPlayableDirectorStopped(PlayableDirector aDirector)
+    {
+        obj.SetActive(false);
+    }
 
 
 
